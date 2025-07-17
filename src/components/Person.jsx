a5ac77c7894691f7f8abf92477fb474dc2846535
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-17 13:12:54 +0530

    initial commit

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 
 const Person = () => {
     const[person,setPerson]=useState({name:"Abhishek",age:25,city:"Bhopal"});
+    const [flag,setFlag]=useState(false);
   return (
     <div>
-        <h1>{person.city}</h1>
+        {flag?"":<h1>Hide Me</h1>}
+        <button className="border p-2 rounded-2xl" onClick={()=>setFlag(!flag)}>Toggle Here</button>
     </div>
   )
 }
