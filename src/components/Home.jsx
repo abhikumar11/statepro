c2189ed24872ca279f3932c493a338d3d5d42ccf
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-19 12:52:40 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>This is home page</div>
+    <div>
+      <h1>This is home page</h1>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
