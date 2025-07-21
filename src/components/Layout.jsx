fdf1d4076b2e473af87cf50361e1d934ea9e7aee
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-21 12:37:07 +0530

    initial commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div>
-        <nav>
-            <ul>
-                <li><Link  className="text-blue-700 underline" to={"/"}>Home</Link></li>
-                <li><Link  className="text-blue-700 underline" to={"/about"}>About</Link></li>
-                  <li><Link  className="text-blue-700 underline" to={"/services"}>Services</Link></li>
-                <li><Link  className="text-blue-700 underline" to={"/contact"}>Contact</Link></li>
-        
-            </ul>
-        </nav>
-        <Outlet/>
-        <footer>
-            <h1>This is footer</h1>
-        </footer>
-    </div>
-  )
-}
+     return (
+          <div className="min-h-screen flex flex-col">
+               <nav className="bg-teal-600 text-white py-4">
+                    <ul className="flex gap-10 justify-center font-semibold">
+                         <li>
+                              <Link className="px-3 py-2 rounded hover:bg-teal-700 transition" to={"/"}>Home</Link>
+                         </li>
+                         <li>
+                              <Link  className="px-3 py-2 rounded hover:bg-teal-700 transition" to={"/about"}>About</Link>
+                         </li>
+                         <li>
+                              <Link  className="px-3 py-2 rounded hover:bg-teal-700 transition" to={"/services"}>Services</Link>
+                         </li>
+                         <li>
+                              <Link  className="px-3 py-2 rounded hover:bg-teal-700 transition" to={"/contact"}>Contact</Link>
+                         </li>
+                    </ul>
+               </nav>
+               <main className="flex-grow p-4 bg-slate-50 text-slate-800">
+                    <Outlet />
+               </main>
+               <footer className="bg-yellow-100 text-teal-800 text-center py-4 border-t">
+                    <h1 className="text-gray-700">This is footer</h1>
+               </footer>
+          </div>
+     );
+};
 
-export default Layout
+export default Layout;
